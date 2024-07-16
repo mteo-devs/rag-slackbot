15914c784666abb2eb1193aacb5cdbaf970425ee
FD\mteo <mteo@firstderivatives.com>
2024-07-16 23:36:12 +0800

    change slack http to socket mode

--- bolt_bot.py
+++ bolt_bot.py
@@ -1,6 +1,7 @@
 import os
 import embeddings_init
 from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 app = App(
     token=os.environ.get("SLACK_AUTH"),
@@ -22,15 +23,16 @@ def event_test(body, say, logger):
     result = query_engine.query(body['event']['text'])
     say(result.response)
 
-from flask import Flask, request
-from slack_bolt.adapter.flask import SlackRequestHandler
+# from flask import Flask, request
+# from slack_bolt.adapter.flask import SlackRequestHandler
 
-flask_app = Flask(__name__)
-handler = SlackRequestHandler(app)
+# flask_app = Flask(__name__)
+# handler = SlackRequestHandler(app)
 
-@flask_app.route("/slack/events", methods=["POST"])
-def slack_events():
-    return handler.handle(request)
+# @flask_app.route("/slack/events", methods=["POST"])
+# def slack_events():
+#     return handler.handle(request)
 
 if __name__ == "__main__":
-    flask_app.run(host='0.0.0.0', port='5000',debug=True)
+    # flask_app.run(host='0.0.0.0', port='5000',debug=True)
+    SocketModeHandler(app,os.environ.get("SLACK_SOCKET_TOKEN")).start()
